43deb320620fc0f3ee6a82e8d33fff885d553c46
Dan Ott <danott@users.noreply.github.com>
2021-09-16 11:42:05 -0400

    🐛 Fix Netlify build
    Apparently the Netlify runtime doesn't support the null coalescing
    operator. Probably a `node` version thing. I don't care enough to look
    into it for two call sites.

--- site/books/books.11tydata.js
+++ site/books/books.11tydata.js
@@ -2,7 +2,7 @@ module.exports = {
   layout: "book",
   tags: ["books"],
   eleventyComputed: {
-    minimalTitle: (data) => data.title?.split(":")[0],
-    minimalAuthor: (data) => data.author?.split(",")[0],
+    minimalTitle: (data) => (data.title || "").split(":")[0],
+    minimalAuthor: (data) => (data.author || "").split(",")[0],
   },
 }
